f0e090d9fb00326b4c3cbbbde3751218a16592db
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2022-08-20 16:06:51 +0200

    print database url and username after app starting

--- post-service/src/main/java/com/javaworld/instagram/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/javaworld/instagram/postservice/PostServiceApplication.java
@@ -2,14 +2,25 @@ package com.javaworld.instagram.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 //@EnableEurekaClient
 //@EnableCircuitBreaker
 public class PostServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(PostServiceApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(PostServiceApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(PostServiceApplication.class, args);
+
+		String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
+		String mysqlUserName = ctx.getEnvironment().getProperty("spring.datasource.username");
+
+		logger.info("Connected to MySQL: " + mysqlUri + " with Username: " + mysqlUserName);
+
 	}
 
 }
