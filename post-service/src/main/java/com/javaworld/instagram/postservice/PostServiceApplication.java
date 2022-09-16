6ded81779ab72070f3edb979f9e1e219af6aa4d3
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2022-09-16 16:35:42 +0200

    create post is done using schedulers

--- post-service/src/main/java/com/javaworld/instagram/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/javaworld/instagram/postservice/PostServiceApplication.java
@@ -1,8 +1,14 @@
 package com.javaworld.instagram.postservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,15 +19,27 @@ public class PostServiceApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(PostServiceApplication.class);
 
+	@Value("${app.threadPoolSize:10}")
+	private Integer threadPoolSize;
+
+	@Value("${app.taskQueueSize:100}")
+	private Integer taskQueueSize;
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(PostServiceApplication.class, args);
 
-		//print database URL after service starting
+		// print database URL after service starting
 		String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
 		String mysqlUserName = ctx.getEnvironment().getProperty("spring.datasource.username");
 
 		logger.info("Connected to MySQL: " + mysqlUri + " with Username: " + mysqlUserName);
 
+	}
+
+	@Bean
+	public Scheduler jdbcScheduler() {
+		logger.info("Creates a jdbcScheduler with thread pool size = {}", threadPoolSize);
+		return Schedulers.newBoundedElastic(threadPoolSize, taskQueueSize, "jdbc-pool");
 	}
 
 }
--- post-service/src/main/java/com/javaworld/instagram/postservice/features/post/service/PostServiceImpl.java
+++ post-service/src/main/java/com/javaworld/instagram/postservice/features/post/service/PostServiceImpl.java
@@ -1,26 +1,51 @@
 package com.javaworld.instagram.postservice.features.post.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javaworld.instagram.postservice.commons.exceptions.InvalidInputException;
 import com.javaworld.instagram.postservice.features.post.persistence.PostEntity;
 import com.javaworld.instagram.postservice.features.post.persistence.PostRepository;
 
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 @Service
 public class PostServiceImpl implements PostService {
 
+	private static final Logger logger = LoggerFactory.getLogger(PostServiceImpl.class);
+
 	@Autowired
 	private PostRepository postRepository;
+
+	@Autowired
+	@Qualifier("jdbcScheduler")
+	private Scheduler jdbcScheduler;
 
 	@Override
 	@Transactional
 	public Mono<PostEntity> createPost(PostEntity postEntity) {
 		
-		//TODO: use JDBC scheduler that is described in the book
-		return postRepository.save(postEntity);
+		return Mono.fromCallable(() -> internalCreatePost(postEntity))
+				   .subscribeOn(jdbcScheduler);
+	}
+
+	private PostEntity internalCreatePost(PostEntity postEntity) {
+		try {
+
+			PostEntity newEntity = postRepository.save(postEntity);
+			logger.debug("createPost: created a post entity");
+			return newEntity;
+
+		} catch (DataIntegrityViolationException dive) {
+			// TODO: is this error message correct
+			throw new InvalidInputException("Duplicate key");
+		}
 	}
 
 }
