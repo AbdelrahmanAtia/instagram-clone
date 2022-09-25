df492fc34f8a77b4456d8c8bc0e02d113c338269
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2022-09-25 22:11:13 +0200

    Update UserServiceImpl.java

--- user-info-service/src/main/java/com/javaworld/instagram/userinfoservice/service/UserServiceImpl.java
+++ user-info-service/src/main/java/com/javaworld/instagram/userinfoservice/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.javaworld.instagram.userinfoservice.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +18,11 @@ public class UserServiceImpl implements UserService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(UserServiceImpl.class);
 
+	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	public Mono<UserEntity> createUser(UserEntity userEntity) {
-
 
 		Mono<UserEntity> newEntity = userRepository.save(userEntity)
 				.log(LOG.getName(), FINE)
