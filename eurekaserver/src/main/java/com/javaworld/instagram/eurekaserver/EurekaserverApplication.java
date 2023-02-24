50f7c24eeebf8e08b6685563a53e59c485e98b6a
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-02-24 14:49:08 +0200

    removed old eureka service and added new one

--- eurekaserver/src/main/java/com/javaworld/instagram/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/javaworld/instagram/eurekaserver/EurekaserverApplication.java
@@ -1,4 +1,4 @@
-package com.javaworld.eurekaservice;
+package com.javaworld.instagram.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
+public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
 }
