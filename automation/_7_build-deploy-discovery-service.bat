fd2f5b0689ee6511b3fc8a0e0fae133501affb11
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-03-19 00:53:21 +0200

    eureka sever || added configs to eureka to protect it with basic auth

---
+++ automation/_7_build-deploy-discovery-service.bat
@@ -0,0 +1,5 @@
+C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -o  -DskipTests=true  -f ../eureka-server/pom.xml  & ^
+docker compose stop eureka & ^
+docker compose rm & ^
+docker compose build eureka & ^
+docker compose up -d eureka
