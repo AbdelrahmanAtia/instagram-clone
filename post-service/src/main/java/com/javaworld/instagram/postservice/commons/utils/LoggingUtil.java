4bc8bab0afe33f927cb92782833a90e9530c08c7
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-03-26 02:03:03 +0200

    update post service with security config

---
+++ post-service/src/main/java/com/javaworld/instagram/postservice/commons/utils/LoggingUtil.java
@@ -0,0 +1,43 @@
+
+package com.javaworld.instagram.postservice.commons.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import java.net.URL;
+import java.util.List;
+
+public class LoggingUtil {
+
+	private static final Logger LOG = LoggerFactory.getLogger(LoggingUtil.class);
+
+	public static void logAuthorizationInfo(SecurityContext sc) {
+		if (sc != null && sc.getAuthentication() != null && sc.getAuthentication() instanceof JwtAuthenticationToken) {
+			Jwt jwtToken = ((JwtAuthenticationToken) sc.getAuthentication()).getToken();
+			logAuthorizationInfo(jwtToken);
+		} else {
+			LOG.warn("No JWT based Authentication supplied, running tests are we?");
+		}
+	}
+	
+	
+	private static void logAuthorizationInfo(Jwt jwt) {
+		if (jwt == null) {
+			LOG.warn("No JWT supplied, running tests are we?");
+		} else {
+			if (LOG.isDebugEnabled()) {
+				URL issuer = jwt.getIssuer();
+				List<String> audience = jwt.getAudience();
+				Object subject = jwt.getClaims().get("sub");
+				Object scopes = jwt.getClaims().get("scope");
+				Object expires = jwt.getClaims().get("exp");
+
+				LOG.debug("Authorization info: Subject: {}, scopes: {}, expires {}: issuer: {}, audience: {}", subject,
+						scopes, expires, issuer, audience);
+			}
+		}
+	}
+
+}
