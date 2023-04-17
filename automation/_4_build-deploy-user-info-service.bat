1fa9ff771d62454db524625f4ce76bfb9a165adc
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-04-17 18:09:43 +0200

    update automation scripts

--- automation/_4_build-deploy-user-info-service.bat
+++ automation/_4_build-deploy-user-info-service.bat
@@ -1,5 +1,5 @@
-C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install  -DskipTests=true  -f ../user-info-service/pom.xml & ^
-docker compose stop insta-ms-user-info & ^
-docker compose rm & ^
-docker compose build insta-ms-user-info & ^
+C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install  -DskipTests=true  -f ../user-info-service/pom.xml && ^
+docker compose stop insta-ms-user-info && ^
+docker compose rm && ^
+docker compose build insta-ms-user-info && ^
 docker compose up -d insta-ms-user-info
--- automation/_5_build-deploy-post-service.bat
+++ automation/_5_build-deploy-post-service.bat
@@ -1,5 +1,5 @@
-C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -o  -DskipTests=true  -f ../post-service/pom.xml  & ^
-docker compose stop insta-ms-post & ^
-docker compose rm & ^
-docker compose build insta-ms-post & ^
+C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -o  -DskipTests=true  -f ../post-service/pom.xml  && ^
+docker compose stop insta-ms-post && ^
+docker compose rm && ^
+docker compose build insta-ms-post && ^
 docker compose up -d insta-ms-post
