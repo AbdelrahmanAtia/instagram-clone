3b6ba80ba2fc477916d6a3eaf4f4a6b869b1c500
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-05-15 01:52:41 +0300

    clean up

--- gateway/src/main/java/com/javaworld/instagram/gateway/features/management/ManagementApi.java
+++ gateway/src/main/java/com/javaworld/instagram/gateway/features/management/ManagementApi.java
@@ -3,8 +3,6 @@ package com.javaworld.instagram.gateway.features.management;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +13,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.javaworld.instagram.gateway.appconfig.PropertiesConfig;
 
 import reactor.core.publisher.Mono;
-
 
 @RestController
 @RequestMapping("/management")
@@ -32,29 +29,18 @@ public class ManagementApi {
 	public ManagementApi(WebClient.Builder webClientBuilder) {
 		this.webClient = webClientBuilder.build();
 	}
-	
-	/*
-	@PostMapping("/restart")
-	public Mono<String> restartMicroservices(@RequestBody RestartServicesRequest request) {
-		List<String> serviceUrls = getServiceUrls(request.getServiceNames());
-		return Flux.fromIterable(serviceUrls)
-				.flatMap(url -> webClient.post().uri(url + "/actuator/restart").retrieve().toBodilessEntity())
-				.then(Mono.just("All microservices restarted."));
-	}
-	*/
 
 	@GetMapping("/health/{serviceName}")
 	public Mono<String> getServiceHealth(@PathVariable String serviceName) {
-	String uri = getServiceUrl(ServiceName.fromString(serviceName)) + "/actuator/health";
-	logger.info("will call service {} on uri {}", serviceName, uri);
+		String uri = getServiceUrl(ServiceName.fromString(serviceName)) + "/actuator/health";
+		logger.info("will call service {} on uri {}", serviceName, uri);
 
-	
-	return webClient.get().uri(uri)
-	        .retrieve()
-	        .bodyToMono(JsonNode.class)
-	        .map(JsonNode::toString);
+		return webClient.get()
+				.uri(uri)
+				.retrieve()
+				.bodyToMono(JsonNode.class)
+				.map(JsonNode::toString);
 	}
-
 
 	private String getServiceUrl(ServiceName serviceName) {
 		switch (serviceName) {
