214fcedbba2712566ceed81c63287316f9ce1597
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2023-05-15 01:47:35 +0300

    open access to services actuators

--- user-info-service/src/main/java/com/javaworld/instagram/userinfoservice/appconfig/security/SecurityConfig.java
+++ user-info-service/src/main/java/com/javaworld/instagram/userinfoservice/appconfig/security/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.cors().disable().csrf().disable()
+		  .authorizeRequests()
 			.antMatchers("/openapi/**").permitAll()
 			.antMatchers("/webjars/**").permitAll()
 			.antMatchers("/actuator/**").permitAll()
