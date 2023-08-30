d26be6c7c4181a63bb2a09d5f660041f2c2c2177
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-08-31 00:46:56 +0300

    PR-37 || generate post component with no tests

--- insta-clone-web/src/app/app.module.ts
+++ insta-clone-web/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // your components are defined here
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [],  //your services are defined here
   bootstrap: [AppComponent]
 })
 export class AppModule { }
