78653113671ac2d617d874faf931b455c23430e2
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-09-11 00:29:30 +0300

    PR-37 || added more components

---
+++ insta-clone-web/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'insta-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+}
