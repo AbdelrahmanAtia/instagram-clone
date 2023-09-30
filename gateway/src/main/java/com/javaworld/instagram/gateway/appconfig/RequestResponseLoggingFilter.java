c5f06c4c3a1cb5e065b6793585e1793de4202846
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-09-30 14:28:47 +0300

    PR-38 || buffer the request body before logging

--- gateway/src/main/java/com/javaworld/instagram/gateway/appconfig/RequestResponseLoggingFilter.java
+++ gateway/src/main/java/com/javaworld/instagram/gateway/appconfig/RequestResponseLoggingFilter.java
@@ -1,17 +1,25 @@
 package com.javaworld.instagram.gateway.appconfig;
+
+import java.nio.charset.StandardCharsets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.annotation.Order;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
 
 @Component
-@Order(-1) // Specify the order to ensure this filter runs first
 public class RequestResponseLoggingFilter extends AbstractGatewayFilterFactory<RequestResponseLoggingFilter.Config> {
 
     private final Logger logger = LoggerFactory.getLogger(RequestResponseLoggingFilter.class);
+    private static final int MAX_BODY_SIZE = 4096;  // Limit body logging to 4KB
 
     public RequestResponseLoggingFilter() {
         super(Config.class);
@@ -20,12 +28,43 @@ public class RequestResponseLoggingFilter extends AbstractGatewayFilterFactory<R
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            logger.info("Requesttttt: {} {}", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
+            logRequest(exchange);
 
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                logger.info("Responseeeee status code: {}", exchange.getResponse().getStatusCode());
-            }));
+            // Buffer the request body for logging and re-read further in the chain
+            return DataBufferUtils.join(exchange.getRequest().getBody(), MAX_BODY_SIZE)
+            	    .doOnNext(dataBuffer -> {
+            	        if (dataBuffer.readableByteCount() > 0) {
+            	            byte[] bytes = new byte[dataBuffer.readableByteCount()];
+            	            dataBuffer.read(bytes);
+            	            logger.info("Request Body: {}", new String(bytes, StandardCharsets.UTF_8));
+            	        }
+            	    })
+            	    .flatMap(dataBuffer -> {
+            	        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate().build();
+            	        exchange.getAttributes().put(ServerWebExchangeUtils.CACHED_REQUEST_BODY_ATTR, dataBuffer);
+            	        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+            	    })
+            	    .then(Mono.fromRunnable(() -> logResponse(exchange)));
+
         };
+    }
+
+    private void logRequest(ServerWebExchange exchange) {
+        logger.info("----------------------------- Request -----------------------------");
+        ServerHttpRequest request = exchange.getRequest();
+
+        logger.info("Method: {}", request.getMethod());
+        logger.info("URL: {}", request.getURI());
+
+        // Logging Headers
+        MultiValueMap<String, String> headers = request.getHeaders();
+        headers.forEach((key, value) -> logger.info("Header: {}={}", key, value));
+    }
+
+    private void logResponse(ServerWebExchange exchange) {
+        logger.info("----------------------------- Response -----------------------------");
+        logger.info("Status Code: {}", exchange.getResponse().getStatusCode());
+        logger.info("-------------------------------------------------------------------");
     }
 
     public static class Config {
