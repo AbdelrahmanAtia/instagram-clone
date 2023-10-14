c8a8fa779408901cececc4decfb9a199c47288d4
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-10-14 22:39:58 +0300

    PR-43 || added and styled login page

--- insta-clone-web/src/app/app-routing.module.ts
+++ insta-clone-web/src/app/app-routing.module.ts
@@ -3,26 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfilePostsComponent } from './profile-posts/profile-posts.component';
-
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
-    path: '', 
-    component: HomeComponent
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
+    path: 'home', 
+    component: HomeComponent,
     children: [
       {
-        path: 'posts', 
-        component: ProfilePostsComponent
+        path: 'profile',
+        component: ProfileComponent,
+        children: [
+          {
+            path: 'posts', 
+            component: ProfilePostsComponent
+          }
+        ]
       }
     ]
+  },
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+  {
+    path: '', 
+    redirectTo: 'login',
+    pathMatch: 'full'
   }
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- insta-clone-web/src/app/app.module.ts
+++ insta-clone-web/src/app/app.module.ts
@@ -9,23 +9,25 @@ import { ProfileComponent } from './profile/profile.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProfilePostsComponent } from './profile-posts/profile-posts.component'; 
+import { ProfilePostsComponent } from './profile-posts/profile-posts.component';
+import { LoginComponent } from './login/login.component'; 
+import { FormsModule } from '@angular/forms';
+
+
+const COMPONENTS = [ AppComponent,
+  CommentComponent,
+  SidebarComponent,
+  ProfileComponent,
+  FooterComponent,
+  HomeComponent,
+  ProfilePostsComponent,
+  LoginComponent];
+  
+const MODULES = [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule];
 
 @NgModule({
-  declarations: [ // your components are defined here
-    AppComponent,
-    CommentComponent,
-    SidebarComponent,
-    ProfileComponent,
-    FooterComponent,
-    HomeComponent,
-    ProfilePostsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule 
-  ],
+  declarations: [COMPONENTS],
+  imports: [MODULES],
   providers: [],  //your services are defined here
   bootstrap: [AppComponent]
 })
