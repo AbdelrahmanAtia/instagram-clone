d6eca63b7cfceb51c73255d8df1732102f6edab2
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-10-16 01:55:50 +0300

    PR-43 || make creation of the sidebar elements dynamic

--- insta-clone-web/src/app/sidebar/sidebar.component.ts
+++ insta-clone-web/src/app/sidebar/sidebar.component.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
-  //TODO: make creating sidebar element dynamic by looping over 
-  // a list of elements 
+  links = [
+    { label: 'Instagram', icon: null, route: '/' },
+    { label: 'Home', icon: 'fas fa-home', route: '/home' },
+    { label: 'Search', icon: 'fas fa-search', route: '#' },
+    { label: 'Explore', icon: 'fas fa-compass', route: '#' },
+    { label: 'Reels', icon: 'fas fa-film', route: '#' },
+    { label: 'Messages', icon: 'fas fa-envelope', route: '#' },
+    { label: 'Notifications', icon: 'fas fa-heart', route: '#' },
+    { label: 'Create', icon: 'fas fa-plus', route: '#' },
+    { label: 'Profile', icon: 'fas fa-user', route: '/home/profile' },
+    // Add more links as needed
+  ];
+
 
 }
