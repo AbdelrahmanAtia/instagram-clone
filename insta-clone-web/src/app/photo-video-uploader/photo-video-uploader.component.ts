78f8f1b3ee93ae69df87c0d904ca3474305000e2
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-11-19 18:47:56 +0200

    PR-47 || prevent uploading files directly when files are dropped

--- insta-clone-web/src/app/photo-video-uploader/photo-video-uploader.component.ts
+++ insta-clone-web/src/app/photo-video-uploader/photo-video-uploader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '../shared/services/post.service';
 
 @Component({
@@ -6,12 +6,13 @@ import { PostService } from '../shared/services/post.service';
   templateUrl: './photo-video-uploader.component.html',
   styleUrls: ['./photo-video-uploader.component.css']
 })
-export class PhotoVideoUploaderComponent {
+export class PhotoVideoUploaderComponent implements OnInit {
+  
+  private filesToUpload: FileList | null = null;
 
+  constructor(private postService: PostService){}
 
-  constructor(private postService: PostService){
-
-  }
+  ngOnInit(): void {}
 
   onDragOver(event: DragEvent): void {
     event.preventDefault();
@@ -34,19 +35,33 @@ export class PhotoVideoUploaderComponent {
     const files = event.dataTransfer?.files;
 
     if (files && files.length) {
-      //TODO: we need to support uploading multiple files as in instagram
-      const fileToUpload: File = files[0]; // Assuming you want to upload the first file
-      this.postService.uploadFile(fileToUpload).subscribe(
-        response => {
-          console.log('File is uploaded successfully:', response);
-        },
-        error => {
-          console.error('Error uploading file:', error);
-        }
-      );
+      this.filesToUpload = files;
     }
+
   }
   close(): void {
     // logic to close the dialog
+  }
+
+  onPostShare() {
+
+    if(this.filesToUpload == null){
+      //TODO: u need to handle that case
+      return;
+    }
+
+    //TODO: we need to support uploading multiple files as in instagram
+    // Assuming you want to upload the first file
+    const fileToUpload: File = this.filesToUpload[0];
+    
+    this.postService.uploadFile(fileToUpload).subscribe(
+      response => {
+        console.log('File is uploaded successfully:', response);
+      },
+      error => {
+        console.error('Error uploading file:', error);
+      }
+    );
+
   }
 }
