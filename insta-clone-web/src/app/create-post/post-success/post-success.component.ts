2b6d6b6c4d4f12c25e553a4492452bb6a266c2ec
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2023-12-07 15:11:57 +0200

    PR-51 || fixed size of post success dialog

--- insta-clone-web/src/app/create-post/post-success/post-success.component.ts
+++ insta-clone-web/src/app/create-post/post-success/post-success.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'insta-post-success',
   templateUrl: './post-success.component.html',
   styleUrls: ['./post-success.component.css']
 })
-export class PostSuccessComponent {
+export class PostSuccessComponent implements OnInit {
+  
+  constructor(public dialog: MatDialog){}  
+  
+  ngOnInit(): void {
+    this.dialog.getDialogById("upload-media-dialog")?.updateSize("405px", "415px");
+  }
 
 }
