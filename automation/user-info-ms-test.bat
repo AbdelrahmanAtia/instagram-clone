98bbcc427c4c729438e9a545b77095c1ffcbbfc1
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2024-07-27 00:05:23 +0300

    PR-71 || fixed issues in user service impl test

---
+++ automation/user-info-ms-test.bat
@@ -0,0 +1,1 @@
+C:\__apache-maven-3.8.4-for-insta\bin\mvn test -Dspring.profiles.active=test -f ../user-info-service/pom.xml
--- user-info-service/src/test/java/com/javaworld/instagram/userinfoservice/service/UserServiceImplTest.java
+++ user-info-service/src/test/java/com/javaworld/instagram/userinfoservice/service/UserServiceImplTest.java
@@ -1,4 +1,4 @@
-/*
+
 package com.javaworld.instagram.userinfoservice.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,75 +9,58 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javaworld.instagram.userinfoservice.integration.PostServiceIntegration;
 import com.javaworld.instagram.userinfoservice.persistence.FollowerRepository;
 import com.javaworld.instagram.userinfoservice.persistence.UserEntity;
 import com.javaworld.instagram.userinfoservice.persistence.UserRepository;
 import com.javaworld.instagram.userinfoservice.service.dto.User;
-import com.javaworld.instagram.userinfoservice.service.dtomapper.UserMapper;
 import com.javaworld.instagram.userinfoservice.service.dtomapper.UserMapperImpl;
 
-import liquibase.integration.spring.SpringLiquibase;
-
-@SpringBootTest
+@ExtendWith({ MockitoExtension.class, SpringExtension.class })
 @ActiveProfiles("test")
 public class UserServiceImplTest {
 
-    @Mock
-    private UserRepository userRepository;
+	@Mock
+	private UserRepository userRepository;
 
-    @Mock
-    private FollowerRepository followerRepository;
+	@Mock
+	private FollowerRepository followerRepository;
 
-    @Autowired
-    @Qualifier("userMapper")
-    private UserMapper userMapper;
+	@Mock
+	private UserMapperImpl userMapper;
 
-    @Mock
-    private PostServiceIntegration postServiceIntegration;
+	@Mock
+	private PostServiceIntegration postServiceIntegration;
 
-    @Mock
-    private ObjectMapper mapper;
+	@InjectMocks
+	private UserServiceImpl userService;
 
-    @InjectMocks
-    private UserServiceImpl userService;
+	private User user;
+	
+	private UserEntity userEntity;
 
-    private User user;
-    private UserEntity userEntity;
-    
-    @Autowired
-    private ApplicationContext applicationContext;
+	@BeforeEach
+	public void setUp() {
 
-    @BeforeEach
-    public void setUp() {
-    	
-    	System.out.println(">> starting setUp().................");
-        user = new User();
-        user.setUsername("testUser");
+		user = new User();
+		user.setUsername("testUser");
 
-        userEntity = new UserEntity();
-        userEntity.setUsername("testUser");
+		userEntity = new UserEntity();
+		userEntity.setUsername("testUser");
 
-        //when(userMapper.toEntity(user)).thenReturn(userEntity);
-        //when(userMapper.toDto(userEntity)).thenReturn(user);
-        
-        // Print all bean names
-        printAllBeanNames();
-    }
+		when(userMapper.toEntity(user)).thenReturn(userEntity);
+		when(userMapper.toDto(userEntity)).thenReturn(user);
 
-    @Test
+	}
+
+	@Test
     public void testCreateUser_Success() {
         when(userRepository.save(userEntity)).thenReturn(userEntity);
 
@@ -90,61 +73,5 @@ public class UserServiceImplTest {
         verify(userMapper, times(1)).toDto(userEntity);     verify(userMapper, times(1)).toEntity(user);
     }
 
-    
-    @Test
-    public void testCreateUser_DuplicateKeyException() {
-        when(userRepository.save(userEntity)).thenThrow(new DuplicateKeyException("Duplicate key"));
 
-        InvalidInputException exception = assertThrows(InvalidInputException.class, () -> {
-            userService.createUser(user);
-        });
-
-        assertEquals("Duplicate key, UserName: testUser", exception.getMessage());
-        verify(userRepository, times(1)).save(userEntity);
-        verify(userMapper, times(1)).toEntity(user);
-    }
-    
-    
-    private void printAllBeanNames() {
-        String[] beanNames = applicationContext.getBeanDefinitionNames();
-        System.out.println("========== Beans in ApplicationContext ===================");
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-        System.out.println("===========================================================");
-    }    
-    
-    @TestConfiguration
-    static class MyServiceTestConfiguration {
-    	
-        @Bean(name = "userMapper")
-        public UserMapper userMapper() {
-        	System.out.println(">>>>>>>>>>>>>");
-        	
-        	UserMapperImpl userMapperImpl = new UserMapperImpl();
-        	
-        	System.out.println("========= userMapperImpl ============");
-        	System.out.println(userMapperImpl);
-        	System.out.println("=====================================");
-            return userMapperImpl;
-        }
-        
-        
-        
-        @Bean
-        @Primary
-        public SpringLiquibase liquibase() {
-            return new SpringLiquibase() {
-                @Override
-                public void afterPropertiesSet() {
-                    // Do nothing
-                }
-            };
-        } 
-           
-        
-        
-    }
-    
 }
-*/
