0f76917d9bb101dd1a83b3cb3d60bc290f6d2ade
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2024-08-18 20:04:26 +0300

    updated

--- jenkins/jenkins-start.bat
+++ jenkins/jenkins-start.bat
@@ -1,17 +1,9 @@
-@echo off
-REM Build the Docker image
-docker build -t my-jenkins-image .
+docker build -t myjenkins-blueocean:2.462.1-1 .
 
-REM Check if a container named my-jenkins is already running and stop it
-docker ps -a --format "{{.Names}}" | findstr /i "my-jenkins" >nul
-IF %ERRORLEVEL% EQU 0 (
-    echo Stopping and removing existing my-jenkins container...
-    docker stop my-jenkins
-    docker rm my-jenkins
-)
 
-REM Run the Docker container
-docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home --name my-jenkins my-jenkins-image
-
-REM Confirm the container is running
-docker ps --filter "name=my-jenkins"
+docker run --name jenkins-blueocean --restart=on-failure --detach ^
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 ^
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 ^
+  --volume jenkins-data:/var/jenkins_home ^
+  --volume jenkins-docker-certs:/certs/client:ro ^
+  --publish 8080:8080 --publish 50000:50000 myjenkins-blueocean:2.462.1-1
