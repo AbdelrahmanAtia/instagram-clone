fe6df73705c7b5cf52b450e81e94ed2e2e38f203
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2024-08-25 18:41:13 +0300

    added empty lines to retrigger the build

--- jenkins/jenkins-start.bat
+++ jenkins/jenkins-start.bat
@@ -1,3 +1,26 @@
+
+
+@echo off
+REM Check if the network exists
+docker network ls | findstr /r "\bjenkins\b" >nul
+IF ERRORLEVEL 1 (
+    REM Create the network if it doesn't exist
+    docker network create jenkins
+    echo Network 'jenkins' created.
+) ELSE (
+    echo Network 'jenkins' already exists.
+)
+
+
+docker run --name jenkins-docker --rm --detach ^
+  --privileged --network jenkins --network-alias docker ^
+  --env DOCKER_TLS_CERTDIR=/certs ^
+  --volume jenkins-docker-certs:/certs/client ^
+  --volume jenkins-data:/var/jenkins_home ^
+  --publish 2376:2376 ^
+  docker:dind
+
+
 docker build -t myjenkins-blueocean:2.462.1-1 .
 
 
