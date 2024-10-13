b42f2b6efbb49b72652c06a83afed6c255a8f64c
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2024-10-13 18:00:50 +0300

    PR-73 || added search component

---
+++ insta-clone-web/src/app/serach/serach.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'insta-serach',
+  templateUrl: './serach.component.html',
+  styleUrls: ['./serach.component.css']
+})
+export class SerachComponent {
+
+}
