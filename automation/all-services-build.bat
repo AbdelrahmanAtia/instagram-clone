deafd9a82bdd205813511aa4ec7e89ff9fb177e1
abdelrahman_attya <abdelrahmanattya94@gmail.com>
2024-11-16 22:43:13 +0200

    PR-74 ||  updated automation scripts

--- automation/all-services-build.bat
+++ automation/all-services-build.bat
@@ -1,3 +1,4 @@
+C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -DskipTests=true -f ../insta-common-lib/pom.xml && ^
 C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -DskipTests=true -f ../post-service/pom.xml && ^
 C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -DskipTests=true -f ../user-info-service/pom.xml && ^
 C:\__apache-maven-3.8.4-for-insta\bin\mvn clean install -DskipTests=true -f ../eureka-server/pom.xml && ^
